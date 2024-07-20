7f0dc70f56d2cfdbc69d04eaa17298fed56434c9
Camelonx <janlouiefrancisco@gmail.com>
2024-07-20 15:39:18 +0800

    made Join room and change scenes
    made join room scene and functions that change scenes in buttons like back buttons

--- create_room.gd
+++ create_room.gd
@@ -1,8 +1,8 @@
 extends Control
 
-
-
-
-
 func _on_texture_button_pressed():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://game_room.tscn")
