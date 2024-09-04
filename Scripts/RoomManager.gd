ce9037c9bc7d0cffce7e1a13896060c335213568
Camelonx <janlouiefrancisco@gmail.com>
2024-09-04 19:15:07 +0800

    tried doing FB Real time DB(Failed)
    tried 4 tutorial videos regarding connecting the game to a real time database to save the room name , password , ip , and port but did not worked so i deleted the changes and reverted it back to normal.
    p.s: multiplication still works but only in 1 instance and cannot be found in the 2nd instance

--- Scripts/RoomManager.gd
+++ Scripts/RoomManager.gd
@@ -2,13 +2,16 @@ extends Node
 signal room_created
 # Dictionary to hold room data
 var rooms = {}
+func _ready():
+	pass
 
 # Function to add a new room
-func add_room(name: String, has_password: bool, password: String = ""):
-	rooms[name] = {"has_password": has_password, "password": password}
+func add_room(roomname: String, has_password: bool, password: String = ""):
+	rooms[roomname] = {"has_password": has_password, "password": password}
+	
 	print("Current rooms: " , rooms)
 	emit_signal("room_created")
-
 # Function to retrieve room data
-func get_room_data(name: String):
-	return rooms.get(name, null)  # Return room data or null if it doesn't exist
+func get_room_data(roomname: String):
+	# Return room data or null if it doesn't exist
+	return rooms.get(roomname, null)  
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,5 +1,6 @@
 extends Control
-
+func _ready():
+	pass
 func _on_create_room_pressed():
 	get_tree().change_scene_to_file("res://Scenes/create_room.tscn")
 
